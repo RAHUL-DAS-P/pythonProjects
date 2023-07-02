9d64f0487afca8f8f70f4d43021e8c4c7aca77ff
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-07-02 14:49:46 +0530

    creating food class

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,15 +1,17 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time 
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake")
+screen.tracer(0)
 
 
 snake = Snake()
-
+food = Food()
 game_is_on = True
 screen.listen()
 
@@ -24,6 +26,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
     
 
 
