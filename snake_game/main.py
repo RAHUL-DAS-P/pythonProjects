18fed016d34d68e8b7816ed8391d43c63067f079
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-07-02 15:00:23 +0530

    creating scoreboard

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time 
 
 screen = Screen()
@@ -12,6 +13,8 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
+
 game_is_on = True
 screen.listen()
 
